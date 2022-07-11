3f5da4a6087740fe85f11d4e0513eae3c8ceed9b
mcznh <mariacarolina.mf@gmail.com>
2022-07-11 18:43:32 -0300

    Remove storyboard from project

---
+++ Moovist_V2/Moovist_V2/CustomTableViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+//    
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        
+//    }
+}
